8b9ccf5e4ecc8b5a60df296f7bc2f36806f7b8f0
Irokanade <michaeljs.leong@gmail.com>
2021-04-23 16:20:52 +0800

    Initial Commit

---
+++ hw0303/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  hw0303
+//
+//  Created by michaelleong on 23/04/2021.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
